bc90b7f4ea5205f0af4e77b6de5b2d3891cd2fc4
rajdeepBittu <rajdeepmishra222@gmail.com>
2024-10-13 14:34:16 +0530

    number square with error

---
+++ CW/loops/numbersquare.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //prinying a pattern with n no of rows and m no of column
+    int m;
+    cout<<"enter the no columns you need"<<endl;
+    cin>>m;
+    int n;
+    cout<<"enter the no rows you need"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            cout<<j;
+        };
+        cout<<endl;
+    }
+
+   return 0;
+}
