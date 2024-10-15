59541b7496f7635e04e692fcfa04bfcf1cd6d932
rajdeepBittu <rajdeepmishra222@gmail.com>
2024-10-15 19:38:23 +0530

    with error

---
+++ CW/loops/task_one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"choose the number n"<<endl;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=i;0){
+            cout<<j;
+        };
+        cout<<endl;
+    
+   return 0;
+}
